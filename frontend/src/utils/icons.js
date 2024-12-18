3f99570c723848cf719868c7fe35b36762f02878
shreyas264 <shreyasparab264@gmail.com>
2024-12-18 11:03:30 +0530

    Add the expense tracker file

---
+++ frontend/src/utils/icons.js
@@ -0,0 +1,32 @@
+export const dashboard = <i class="fa-solid fa-house"></i>
+export const transactions = <i class="fa-solid fa-credit-card"></i>
+export const categories = <i class="fa-solid fa-tags"></i>
+export const accounts = <i class="fa-solid fa-wallet"></i>
+export const settings = <i class="fa-solid fa-gear"></i>
+export const logout = <i class="fa-solid fa-right-from-bracket"></i>
+export const trend = <i class="fa-solid fa-money-bill-trend-up"></i>
+export const expenses = <i class="fa-solid fa-money-bill-transfer"></i>
+export const money = <i class="fa-solid fa-money-bill"></i>
+export const freelance = <i class="fa-solid fa-earth-asia"></i>
+export const stocks = <i class="fa-solid fa-arrow-trend-up"></i>
+export const bitcoin = <i class="fa-brands fa-bitcoin"></i>
+export const piggy = <i class="fa-solid fa-piggy-bank"></i>
+export const yt = <i class="fa-brands fa-youtube"></i>
+export const card = <i class="fa-brands fa-cc-visa"></i>
+export const users = <i class="fa-solid fa-users-between-lines"></i>
+export const rupee = <i class="fa-solid fa-indian-rupee-sign"></i>
+export const calender = <i class="fa-solid fa-calendar"></i>
+export const comment = <i class="fa-solid fa-comment"></i>
+export const plus = <i class="fa-solid fa-plus"></i>
+export const trash = <i class="fa-solid fa-trash"></i>
+export const signout = <i class="fa-solid fa-right-from-bracket"></i>
+export const takeaway = <i class="fa-solid fa-utensils"></i> 
+export const clothing = <i class="fa-solid fa-shirt"></i>
+export const book = <i class="fa-solid fa-book-open"></i>
+export const food = <i class="fa-solid fa-bowl-food"></i>
+export const medical = <i class="fa-solid fa-briefcase-medical"></i> 
+export const tv = <i class="fa-solid fa-tv"></i>
+export const circle = <i class="fa-solid fa-circle-dot"></i>
+
+
+
---
+++ frontend/src/utils/menuItems.js
@@ -0,0 +1,20 @@
+export const menuItems = [
+    {
+        id:1,
+        title: 'Dashboard',
+        icon: ' ',
+        link: '/dashboard'
+    },
+    {
+        id:2,
+        title: 'View Transactions',
+        icon: ' ',
+        link: '/dashboard'
+    },
+    {
+        id:3,
+        title: 'Incomes',
+        icon: ' ',
+        link: '/dashboard'
+    },
+]
